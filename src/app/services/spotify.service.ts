cac52fd7f8b4f52b216a99ab2b7586ef5731f6ed
Vitaly Kiselev <st411ar@ya.ru>
2018-09-19 14:21:17 +0300

    created spotify service

---
+++ src/app/services/spotify.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+
+import { SpotifyAPIKey } from '../../environments/spotifyApiKey';
+
+
+@Injectable()
+export class SpotifyService {
+
+  constructor(private http: HttpClient) {}
+
+
+  searchTrack(query: string): Observable<any> {
+    const params: string = [
+      `q=${query}`,
+      `type=track`
+    ].join(`&`);
+
+    const queryURL = `https://api.spotify.com/v1/search?${params}`;
+
+    return this.http.get(queryURL, this.buildRequestOptions())
+  }
+
+
+  private buildRequestOptions(): any {
+    const headers: HttpHeaders = new HttpHeaders({
+      Authorization: `Bearer ${SpotifyAPIKey}`
+    });
+    return { headers: headers };
+  }
+}
