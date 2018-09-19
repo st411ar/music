c356c38c0e2255d4c3b9e86ec3c49737155d5790
Vitaly Kiselev <st411ar@ya.ru>
2018-09-19 15:55:56 +0300

    implemented submit search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 
+import { SpotifyService } from '../services/spotify.service';
+
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html'
@@ -8,7 +11,10 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class SearchComponent implements OnInit {
   query: string;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(
+      private route: ActivatedRoute,
+      private spotify: SpotifyService
+  ) {
     this.route.queryParams.subscribe(
       (params: Params) => {
         this.query = params[`query`] || ``;
@@ -17,5 +23,17 @@ export class SearchComponent implements OnInit {
   }
 
   ngOnInit() {}
+
+
+  submit(query: string): void {
+    if (this.query) {
+      this.spotify.searchTrack(this.query)
+        .subscribe(
+          (results: any) => {
+            console.log(results);
+          }
+        );
+    }
+  }
 
 }
