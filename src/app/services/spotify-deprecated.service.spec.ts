57d13afe499caed71133298a35f11a31b2d5d2d6
Vitaly Kiselev <st411ar@ya.ru>
2018-09-21 17:53:41 +0300

    added spec with injection

--- src/app/services/spotify-deprecated.service.spec.ts
+++ src/app/services/spotify-deprecated.service.spec.ts
@@ -1,12 +1,59 @@
-import { TestBed } from '@angular/core/testing';
+import {
+  BaseRequestOptions,
+  ConnectionBackend,
+  Http
+} from '@angular/http';
+
+import { MockBackend } from '@angular/http/testing';
+
+import {
+  inject,
+  TestBed
+} from '@angular/core/testing';
 
 import { SpotifyDeprecatedService } from './spotify-deprecated.service';
 
 describe('SpotifyDeprecatedService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        SpotifyDeprecatedService,
+        BaseRequestOptions,
+        MockBackend,
+        {
+          provide: Http,
+          useFactory: (
+            backend: ConnectionBackend,
+            defaultOptions: BaseRequestOptions
+          ) => {
+            return new Http(backend, defaultOptions);
+          },
+          deps: [
+            MockBackend,
+            BaseRequestOptions
+          ]
+        }
+      ],
+    });
+  });
 
   it('should be created', () => {
     const service: SpotifyDeprecatedService = TestBed.get(SpotifyDeprecatedService);
     expect(service).toBeTruthy();
+  });
+
+  describe('getTrack', () => {
+    it(
+      'retrieves using the track ID',
+      inject(
+        [
+          SpotifyDeprecatedService,
+          MockBackend
+        ],
+        (spotify: SpotifyDeprecatedService, backend: MockBackend) => {
+          console.log('logic with injections');
+        }
+      )
+    );
   });
 });
